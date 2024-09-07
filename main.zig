8dfa14c77831414982bfcc6178afa18050db501e
Nathan Mack <nathan@macktribe.com>
2024-09-07 15:46:17 -0500

    Remove raylib
    The bindings were outdated, and it was too much trouble to get them working...

--- main.zig
+++ main.zig
@@ -147,22 +147,7 @@ fn decode_op_code(code: u16) void {
 }
 
 pub fn main() void {
-    // std.debug.print("Hello, {s}!\n", .{"World"});
-    raylib.SetConfigFlags(raylib.ConfigFlags{ .FLAG_WINDOW_RESIZABLE = true });
-    raylib.InitWindow(800, 800, "hello world!");
-    raylib.SetTargetFPS(60);
-
-    defer raylib.CloseWindow();
-
-    while (!raylib.WindowShouldClose()) {
-        raylib.BeginDrawing();
-        defer raylib.EndDrawing();
-
-        raylib.ClearBackground(raylib.BLACK);
-        raylib.DrawFPS(10, 10);
-
-        raylib.DrawText("hello world!", 100, 100, 20, raylib.YELLOW);
-    }
+    std.debug.print("Hello, {s}!\n", .{"World"});
 }
 
 //--------------------------------------------------------------//
